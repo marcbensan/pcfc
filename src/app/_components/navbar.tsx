26beb64024c69584c68c592bb9a466d2e9c4aef9
Marc <139300806+marcbensan@users.noreply.github.com>
2024-12-02 21:22:08 -0500

    feat: add mobile view for navbar

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -2,7 +2,8 @@
 
 import * as React from "react";
 import Link from "next/link";
-
+import { MenuItem } from "@/lib/types/navbar";
+import { Bars2Icon } from "@heroicons/react/24/outline";
 import {
   NavigationMenu,
   NavigationMenuItem,
@@ -10,28 +11,87 @@ import {
   NavigationMenuList,
 } from "@/components/ui/navigation-menu";
 
-const navBarItems = [
-  { id: 1, name: "About", href: "/about" },
-  { id: 2, name: "Sermons", href: "/sermons" },
-  { id: 3, name: "Ministries", href: "/ministries" },
-  { id: 4, name: "Contact", href: "/contact" },
-  { id: 5, name: "Giving", href: "/giving" },
+import { cn } from "@/lib/utils";
+import {
+  Sheet,
+  SheetContent,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { Collapsible } from "@/components/ui/collapsible";
+
+const navBarItems: MenuItem[] = [
+  { name: "About", href: "/about" },
+  { name: "Sermons", href: "/sermons" },
+  { name: "Ministries", href: "/ministries" },
+  { name: "Contact", href: "/contact" },
+  { name: "Giving", href: "/giving" },
 ];
 
 export function NavigationMenuDemo() {
   return (
-    <NavigationMenu className="bg-zinc-800 w-full">
-      <NavigationMenuList className="px-4 pt-6 pb-1">
-        {navBarItems.map((item) => (
-          <NavigationMenuItem key={item.id} className="relative">
-            <Link href={item.href} legacyBehavior passHref>
-              <NavigationMenuLink className="inline-flex h-9 w-max items-center justify-center px-4 py-2 text-md hover:border-b-2 hover:border-white font-bold text-white">
-                {item.name}
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-        ))}
-      </NavigationMenuList>
-    </NavigationMenu>
+    <>
+      <NavigationMenu className="bg-zinc-800 w-full hidden md:flex">
+        <NavigationMenuList className="px-4 pt-6 pb-1">
+          {navBarItems.map((item) => (
+            <NavigationMenuItem key={item.name} className="relative">
+              <Link href={item.href} legacyBehavior passHref>
+                <NavigationMenuLink className="inline-flex h-9 w-max items-center justify-center px-4 py-2 text-md hover:border-b-2 hover:border-white font-bold text-white">
+                  {item.name}
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+          ))}
+        </NavigationMenuList>
+      </NavigationMenu>
+
+      {/* MOBILE VIEW */}
+      <HamburgerMenu />
+    </>
+  );
+}
+
+function HamburgerMenu(): JSX.Element {
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <div className="bg-zinc-800 w-full py-3 md:hidden">
+      <Sheet open={open} onOpenChange={setOpen}>
+        <SheetTrigger asChild>
+          <Bars2Icon className="size-10 text-white pl-4" />
+        </SheetTrigger>
+        <SheetContent side="left" className="w-[240px] sm:w-[300px]">
+          <SheetTitle className="sr-only">Accessible Title</SheetTitle>
+          <nav className="flex flex-col space-y-3 pt-4">
+            {navBarItems.map((item) => (
+              <MenuItemComponent key={item.name} item={item} />
+            ))}
+          </nav>
+        </SheetContent>
+      </Sheet>
+    </div>
+  );
+}
+
+function MenuItemComponent({
+  item,
+}: Readonly<{ item: MenuItem }>): JSX.Element {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  return (
+    <Collapsible open={isOpen} onOpenChange={setIsOpen}>
+      <Link href={item.href}>
+        <Button
+          variant="ghost"
+          className={cn(
+            "flex w-full items-center justify-between py-2 text-md font-bold transition-colors hover:text-primary"
+          )}
+        >
+          {item.name}
+        </Button>
+      </Link>
+    </Collapsible>
   );
 }
