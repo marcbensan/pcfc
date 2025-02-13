c254a439b8ffc0cf9f71d59ce27c38fc66283596
Marc <139300806+marcbensan@users.noreply.github.com>
2025-02-12 22:49:24 -0500

    fix: resize accordion

--- src/app/about/_components/values.tsx
+++ src/app/about/_components/values.tsx
@@ -9,22 +9,26 @@ import { values } from "@/lib/data/values";
 export default function Values(): JSX.Element {
   return (
     <section className="diagonal-section relative z-[1] bg-primarypcfc">
-      <div className="mx-24 py-44">
+      <div className="mx-24 space-y-16 py-44">
         <p className="whitespace-nowrap text-center font-monaSans text-xl font-bold text-white md:text-8xl">
           OUR VALUES
         </p>
-        <Accordion type="single" collapsible className="w-full text-white">
-          {values.map((value) => (
-            <div key={value.title}>
-              <AccordionItem value={value.title}>
-                <AccordionTrigger className="font-bold">
-                  {value.title}
-                </AccordionTrigger>
-                <AccordionContent>{value.description}</AccordionContent>
-              </AccordionItem>
-            </div>
-          ))}
-        </Accordion>
+        <div className="flex justify-center">
+          <Accordion type="single" collapsible className="w-[75%] text-white">
+            {values.map((value) => (
+              <div key={value.title}>
+                <AccordionItem value={value.title}>
+                  <AccordionTrigger className="py-8 text-2xl font-bold">
+                    {value.title}
+                  </AccordionTrigger>
+                  <AccordionContent className="text-xl">
+                    {value.description}
+                  </AccordionContent>
+                </AccordionItem>
+              </div>
+            ))}
+          </Accordion>
+        </div>
       </div>
     </section>
   );
