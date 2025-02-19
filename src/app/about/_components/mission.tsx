cfc2ae8c135562901231b2f0763d10a7a2cb25ec
Marc <139300806+marcbensan@users.noreply.github.com>
2025-02-19 16:14:51 -0500

    fix: fix mission in mobile view

--- src/app/about/_components/mission.tsx
+++ src/app/about/_components/mission.tsx
@@ -2,22 +2,20 @@ import Image from "next/image";
 
 export default function Mission(): JSX.Element {
   return (
-    <div className="flex flex-col items-center justify-center space-x-24 md:h-[1000px] md:flex-row md:px-24">
+    <div className="my-24 flex flex-col-reverse items-center justify-center px-8 md:my-0 md:h-[1000px] md:flex-row md:space-x-24 md:px-24">
       <div className="flex h-full w-full max-w-[1000px] animate-fadeUp items-center justify-center">
-        <p className="text-start font-barlow font-bold italic text-black md:text-8xl md:text-[80px]">
+        <p className="my-8 text-center font-barlow text-5xl font-bold italic text-black md:my-0 md:text-start md:text-8xl md:text-[80px]">
           To see those that God loves, come back to His family
         </p>
       </div>
-      <div>
-        <div className="flex h-[100px] w-[100px] items-center justify-center overflow-hidden rounded-full md:h-[600px] md:w-[600px]">
-          <Image
-            alt="mission-img"
-            src="/mission.jpg"
-            width={1000}
-            height={1000}
-            className="h-full w-full object-cover"
-          />
-        </div>
+      <div className="flex h-[250px] w-[250px] items-center justify-center overflow-hidden rounded-full md:h-[500px] md:w-[900px]">
+        <Image
+          alt="mission-img"
+          src="/mission.jpg"
+          width={1000}
+          height={1000}
+          className="h-full w-full object-cover"
+        />
       </div>
     </div>
   );
