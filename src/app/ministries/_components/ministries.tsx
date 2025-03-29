eb2319baaf5dcd7c1f4b3a8324d3a175d8565cb5
Marc <139300806+marcbensan@users.noreply.github.com>
2025-03-28 23:28:41 -0400

    fix: ministry images

--- src/app/ministries/_components/ministries.tsx
+++ src/app/ministries/_components/ministries.tsx
@@ -2,9 +2,48 @@ import { ThreeDMarquee } from "@/components/ui/3d-hero";
 import { ministries } from "@/lib/data/ministries";
 import MinistryContainer from "./ministry-container";
 
-export default function Ministries({ images }: { images: string[] }) {
+export default function Ministries() {
+  const images = [
+    "/ministries/_MG_6051.jpg",
+    "/ministries/_MG_6060.jpg",
+    "/ministries/_MG_9621-Enhanced-NR.jpg",
+    "/ministries/_MG_9646-Enhanced-NR.jpg",
+    "/ministries/DSC_1284.JPG",
+    "/ministries/DSC_1479.JPG",
+    "/ministries/DSC_1629.jpg",
+    "/ministries/DSC_2335.jpg",
+    "/ministries/DSC_2583.jpg",
+    "/ministries/DSC_3206.jpg",
+    "/ministries/DSC_3595.JPG",
+    "/ministries/DSC_3640.JPG",
+    "/ministries/DSC_3666.JPG",
+    "/ministries/DSC_3686.JPG",
+    "/ministries/DSC_3755.JPG",
+    "/ministries/DSC_3782.JPG",
+    "/ministries/DSC_3935.JPG",
+    "/ministries/DSC_8639.jpg",
+    "/ministries/DSC_9601.JPG",
+    "/ministries/DSC_9717.JPG",
+    "/ministries/DSC00596.JPG",
+    "/ministries/DSC05041.JPG",
+    "/ministries/DSC05126.JPG",
+    "/ministries/DSC05239.JPG",
+    "/ministries/DSC05313.JPG",
+    "/ministries/DSC05331.JPG",
+    "/ministries/DSC05432.jpg",
+    "/ministries/DSC05449.jpg",
+    "/ministries/DSC05457.jpg",
+    "/ministries/DSC05532.jpg",
+    "/ministries/DSC05934.jpg",
+    "/ministries/DSC06065.jpg",
+    "/ministries/DSC06125.jpg",
+    "/ministries/DSC06317.JPG",
+    "/ministries/DSC09825.JPG",
+    "/ministries/IMG_5937.JPG",
+    "/ministries/IMG_9426.jpg",
+  ];
   return (
-    <div className="py-32">
+    <div className="py-40">
       <p className="pb-12 text-center font-monaSans text-5xl font-bold md:text-8xl">
         OUR MINISTRIES
       </p>
--- src/app/ministries/page.tsx
+++ src/app/ministries/page.tsx
@@ -1,15 +1,5 @@
 import Ministries from "./_components/ministries";
 
-import fs from "fs";
-import path from "path";
-
-export default function MinistryPage() {
-  const imagesDirectory = path.join(process.cwd(), "public/ministries");
-  const imageFiles = fs.readdirSync(imagesDirectory);
-
-  const images = imageFiles.map((file) => `/ministries/${file}`);
-
-  console.log(images.length);
-
-  return <Ministries images={images} />;
+export default async function MinistryPage() {
+  return <Ministries />;
 }
