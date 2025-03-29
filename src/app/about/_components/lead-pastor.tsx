e26bd2db6b088cbce20f34cd0a5a2651864c115a
Marc <139300806+marcbensan@users.noreply.github.com>
2025-03-28 22:04:59 -0400

    feat: add 3d for ministries

--- src/app/about/_components/lead-pastor.tsx
+++ src/app/about/_components/lead-pastor.tsx
@@ -1,22 +1,21 @@
-import { imgLink } from "@/lib/data/teamMembers";
 import Image from "next/image";
 
 export default function LeadPastor() {
   return (
-    <section className="diagonal-section-bottom--flip-x diagonal-section-top--flip-x relative z-[1] bg-primarypcfc px-8 py-32">
+    <section className="diagonal-section-bottom--flip-x diagonal-section-top--flip-x relative z-[1] bg-primarypcfc py-32">
       <div className="mx-4 space-y-16 py-44 md:mx-24 md:space-y-0">
-        <div className="flex flex-col space-y-12 md:flex-row md:space-x-24 md:space-y-0">
+        <div className="flex flex-col items-center space-y-12 lg:flex-row lg:space-x-16 lg:space-y-0">
           <Image
             alt="images"
             width={500}
             height={1000}
-            src={imgLink}
-            className="rounded-3xl shadow-lg"
+            src="/people/mark-cheng.jpg"
+            className="rounded-xl shadow-lg lg:h-[30rem]"
           />
-          <div className="flex flex-col space-y-8 font-monaSans text-white">
-            <p className="text-4xl font-bold md:text-6xl">OUR LEADERSHIP</p>
-            <p className="text-2xl font-semibold">
-              PASTOR MARK AND RITCHES DOLOR
+          <div className="flex flex-col items-center space-y-8 text-center text-white lg:items-start lg:text-start">
+            <p className="font-homenaje text-xl md:text-xl">OUR LEADERSHIP</p>
+            <p className="font-barlow text-5xl font-semibold lg:text-7xl">
+              MARK & RITCHES DOLOR
             </p>
             <p className="font-barlow text-xl">
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam
@@ -27,17 +26,6 @@ export default function LeadPastor() {
               quaerat optio, unde rem vel voluptas nulla facere, impedit
               nesciunt doloremque autem necessitatibus magnam veniam iusto
               alias. Aspernatur? Lorem ipsum dolor sit amet consectetur,
-              adipisicing elit. Aliquam molestiae minus beatae quaerat optio,
-              unde rem vel voluptas nulla facere, impedit nesciunt doloremque
-              autem necessitatibus magnam veniam iusto alias. Aspernatur? Lorem
-              ipsum dolor sit amet consectetur, adipisicing elit. Aliquam
-              molestiae minus beatae quaerat optio, unde rem vel voluptas nulla
-              facere, impedit nesciunt doloremque autem necessitatibus magnam
-              veniam iusto alias. Aspernatur? Lorem ipsum dolor sit amet
-              consectetur, adipisicing elit. Aliquam molestiae minus beatae
-              quaerat optio, unde rem vel voluptas nulla facere, impedit
-              nesciunt doloremque autem necessitatibus magnam veniam iusto
-              alias. Aspernatur?
             </p>
           </div>
         </div>
